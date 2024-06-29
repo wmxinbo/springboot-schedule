fd421e4bca01924359e96b8a3bed79da6f832924
xinbo.wang <xinbo.wang@163.com>
2024-06-29 22:07:00 +0800

    TEST BY WXB

---
+++ src/main/java/xinbo/wang/Test.java
@@ -0,0 +1,10 @@
+package xinbo.wang;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello IDEA!");
+        for (int j = 0; j < 10; j++) {
+            System.out.printf("-> ", ++j);
+        }
+    }
+}
